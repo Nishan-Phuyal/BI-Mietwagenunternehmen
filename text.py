7c2d222b9a633e3728f0781e2af97adca912aaf6
Nishan phuyal <phuyal.nishan@gmail.com>
2024-10-26 12:44:09 +0200

    Projektbeschreibung Upload

---
+++ text.py
@@ -0,0 +1,17 @@
+import streamlit as st
+text = """  
+Digitale Analyse-App zur Fahrer- und Unternehmensperformance für Mietwagen- und Taxiunternehmen
+In der zunehmend digitalisierten Welt gewinnen automatisierte Prozesse an Bedeutung – insbesondere im Bereich der Personenbeförderung. Plattformen wie Uber, Bolt und FreeNow sind Vorreiter in der digitalen Abwicklung von Fahrten und stellen Unternehmern regelmäßig Berichte zur Fahrerperformance und Umsätzen zur Verfügung. Diese Berichte sind jedoch oft unstrukturiert und schwer zu interpretieren, was es für viele Unternehmer schwierig macht, einen klaren Überblick über ihr Geschäft zu erhalten.
+Vor diesem Hintergrund habe ich, im Rahmen meiner ehrenamtlichen Tätigkeit bei LCC Hessen, eine App entwickelt, die speziell auf die Bedürfnisse der Mietwagen- und Taxiunternehmer unserer Community zugeschnitten ist. Diese App automatisiert die Verarbeitung und Analyse der csv-Daten und bietet den Unternehmern eine leicht verständliche, umfassende Übersicht über Fahrerleistungen, Umsätze und andere wichtige Kennzahlen.\n
+#### :green[Funktionalitäten der App:]\n
+:red[•	Automatische Berichterstellung: ]\n
+Unternehmer laden lediglich ihre csv-Dateien hoch, und die App erstellt sofort einen detaillierten Bericht, der die Auszahlungssummen pro Fahrer sowie wichtige Performance-Metriken übersichtlich darstellt.\n
+:red[•	Betrugserkennung via Standortanalyse:]\n
+ Eine der bedeutendsten Funktionen der App ist die visuelle Darstellung von Fahrtrouten auf einer Karte. Hiermit lassen sich potenzielle Betrugsfälle identifizieren. Ein häufiges Szenario in der Mietwagenbranche ist, dass Fahrer Langstreckenfahrten offline abschließen und so dem Unternehmer wichtige Einnahmen vorenthalten. Mithilfe der Kartenfunktion können solche Abweichungen sofort sichtbar gemacht werden. Dies hat bereits dazu geführt, dass Unternehmer betrügerische Fahrer identifizieren und entsprechende Maßnahmen ergreifen konnten.\n
+
+ :red[Veröffentlichung und Datenschutz:]\n
+   Ein Teil der App ist öffentlich zugänglich gemacht worden, allerdings werden hierbei ausschließlich Pseudo-Daten verwendet, die zur reinen Darstellung dienen. Die App ist so konzipiert, dass sie strengste Datenschutzanforderungen erfüllt und alle vertraulichen Daten der Unternehmer und Fahrer geschützt bleiben.\n
+ :red[Notiz:]\n
+   Dieses Projekt war im Vergleich zu anderen technisch weniger komplex, entpuppte sich jedoch als eines der nützlichsten Tools für viele Taxi- und Mietwagenunternehmer. Es steigert nicht nur die betriebliche Effizienz, sondern trägt auch signifikant zur Erkennung von betrügerischem Verhalten bei. Die App unterstützt Unternehmer dabei, ihre Geschäftsprozesse zu optimieren und vor finanziellen Verlusten geschützt zu sein.
+
+"""
